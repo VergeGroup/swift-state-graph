9590e2dc4e4c3437cf8f2ae059f0f98d277ee688
Muukii <muukii.app@gmail.com>
2025-05-04 21:27:11 +0900

    Update

--- Sources/StateGraph/StateView.swift
+++ Sources/StateGraph/StateView.swift
@@ -15,56 +15,6 @@ extension StateViewType {
   }
 }
 
-//open class StateView: Hashable, StateViewType {
-//
-//  public func hash(into hasher: inout Hasher) {
-//    hasher.combine(ObjectIdentifier(self))
-//  }
-//
-//  public static func == (lhs: StateView, rhs: StateView) -> Bool {
-//    return lhs === rhs
-//  }
-//
-//  public init() {
-//  }
-//
-//  private let lock: OSAllocatedUnfairLock<Void> = .init()
-//  nonisolated(unsafe)
-//    private var _sink: Sink<Void> = .init()
-//
-//  public func onChange() -> AsyncStream<Void> {
-//    lock.lock()
-//    defer {
-//      lock.unlock()
-//    }
-//    return _sink.addStream()
-//  }
-//
-//  func _onMemberChange() {
-//
-//    onMemberChange()
-//
-//    lock.lock()
-//    defer {
-//      lock.unlock()
-//    }
-//    _sink.send(output: ())
-//
-//  }
-//  
-//  open func onMemberChange() {
-//
-//  }
-//
-//}
-
-extension StateViewType {
-
-//  public typealias Computed<Value> = ComputedMember<Value>
-//  public typealias Stored<Value> = StoredMember<Value>
-
-}
-
 #if canImport(SwiftUI)
 
   import SwiftUI
