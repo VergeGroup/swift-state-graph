764beb40c33ad29509011ed97d61c2cb0c3a8fbd
Hiroshi Kimura <muukii.app@gmail.com>
2025-05-23 12:49:07 +0900

    Refactor macro helper (#19)
    ## Summary
    - fix `providingMacros` in macro plugin
    - return new initializer bindings in helper method
    
    ## Testing
    - `swift test` *(fails: no such module 'os.lock')*

--- Sources/StateGraphMacro/Extension.swift
+++ Sources/StateGraphMacro/Extension.swift
@@ -101,9 +101,7 @@ extension VariableDeclSyntax {
       return binding
     }
         
-    return self.with(\.bindings, .init(self.bindings.map {
-      $0.with(\.initializer, initializer)
-    }))
+    return self.with(\.bindings, .init(newBindings))
     
   }
 
