f9b89011088f10feccd47a778566131465dd9237
Muukii <muukii.app@gmail.com>
2025-10-21 04:47:26 +0900

    remove log

--- Sources/StateGraph/util.swift
+++ Sources/StateGraph/util.swift
@@ -3,12 +3,9 @@ import Foundation
 func withMainActor(_ closure: sending @escaping @MainActor () -> Void) {
   
   if #available(iOS 26, macOS 26, watchOS 26, tvOS 26, *) {
-    print("enter")
     Task.immediate { @MainActor in
-      print("inside")
       closure()
     }
-    print("leave")
   } else {    
     if Thread.isMainThread {
       MainActor.assumeIsolated {
